0efcf1b4e8c0c75eb4d174362f4aef148bf6ce9c
Gowri Gary Shankar <gowrishankar_m@hotmail.com>
2014-04-19 12:54:13 +0530

    Stubs for app, device and device param creation added.

---
+++ src/main/java/com/prodapt/m2m/da/core/DeviceSimulator.java
@@ -0,0 +1,36 @@
+/* 
+* @Author: Gowri Gary Shankar
+* @Date:   2014-04-19 12:45:13
+* @Last Modified by:   Gowri Gary Shankar
+* @Last Modified time: 2014-04-19 12:51:20
+*/
+package com.prodapt.m2m.da.core;
+
+import com.prodapt.m2m.rest.domain.Application;
+import com.prodapt.m2m.rest.domain.Container;
+import com.prodapt.m2m.rest.domain.ContentInstance;
+
+
+public class DeviceSimulator {
+
+
+    
+
+	public void createApplication() {
+
+		Application newApp = new Application();
+
+	}
+
+	public void createDevice() {
+
+		Container newContainer = new Container();
+
+	}
+
+	public void sendDeviceParameters() {
+
+		ContentInstance contentInstance = new ContentInstance();
+
+	}
+}
--- src/main/java/com/prodapt/m2m/rest/domain/Application.java
+++ src/main/java/com/prodapt/m2m/rest/domain/Application.java
@@ -59,38 +59,4 @@ public class Application {
 	public String getNotificationChannelReference() {
 		return notificationChannelReference;
 	}
-	
-	/*public ApplicationDetails toApplicationDetails(final String appId) {
-
-		ApplicationDetails details =  new ApplicationDetails();
-		details.setAppId(appId);
-		details.setAccessRightID(accessRightID);
-		details.setSearchStrings(searchStrings);
-		details.setCreationTime(creationTime);
-		details.setLastModifiedTime(lastModifiedTime);
-		details.setaPoC(aPoC);
-		details.setaPocPaths(aPocPaths);
-		
-		return details;
-
-	}
-
-	public static Application fromApplicationDetails(ApplicationDetails appDetails) {
-		
-		Application application = new Application();
-
-		application.appId = appDetails.getAppId();
-		application.accessRightID = appDetails.getAccessRightID();
-		application.searchStrings = appDetails.getSearchStrings();
-		application.creationTime = appDetails.getCreationTime();
-		application.lastModifiedTime = appDetails.getLastModifiedTime();
-		application.aPoC = appDetails.getaPoC();
-		application.aPocPaths = appDetails.getaPocPaths();
-		application.containersReference = appDetails.getContainersReference();
-		application.groupsReference = appDetails.getGroupsReference();
-		application.accessRightsReference = appDetails.getAccessRightsReference();
-		application.notificationChannelReference = appDetails.getNotificationChannelReference();
-
-		return application;
-	}*/
 }
